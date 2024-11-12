93e758c4deccc93284128f45bbda76a15ee048ca
Talather <atherjawad11@gmail.com>
2024-11-13 02:23:57 +0500

    frontend modification

--- src/pages/quiz/quiz.jsx
+++ src/pages/quiz/quiz.jsx
@@ -4,7 +4,8 @@ import { Button, Progress, Radio, RadioGroup } from "@nextui-org/react"
 import { useEffect, useState } from "react"
 import { Controller, useForm } from "react-hook-form"
 import styles from "./quiz.module.css"
-
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import "./slideComponent.css"
 export default function Quiz() {
   const [completed, setCompleted] = useState(false)
   const {
@@ -21,36 +22,40 @@ export default function Quiz() {
       _id: 1,
       name: "Is he a good person?",
       answers: [
-        { _id: 1, name: "Yes" },
-        { _id: 2, name: "No" },
-        { _id: 3, name: "Definitely not" },
-        { _id: 4, name: "No doubt" },
+        { _id: "1", name: "Yes" },
+        { _id: "2", name: "No" },
+        { _id: "3", name: "Definitely not" },
+        { _id: "4", name: "No doubt" },
       ],
+      correctAnswerId: "1",
     },
     {
       _id: 2,
       name: "Will he get a girl in the future?",
       answers: [
-        { _id: 1, name: "Yes" },
-        { _id: 2, name: "No" },
-        { _id: 3, name: "Definitely not" },
-        { _id: 4, name: "No doubt" },
+        { _id: "1", name: "Yes" },
+        { _id: "2", name: "No" },
+        { _id: "3", name: "Definitely not" },
+        { _id: "4", name: "No doubt" },
       ],
+      correctAnswerId: "2",
     },
     {
       _id: 3,
       name: "Will he become rich?",
       answers: [
-        { _id: 1, name: "Yes" },
-        { _id: 2, name: "No" },
-        { _id: 3, name: "Definitely not" },
-        { _id: 4, name: "No doubt" },
+        { _id: "1", name: "Yes" },
+        { _id: "2", name: "No" },
+        { _id: "3", name: "Definitely not" },
+        { _id: "4", name: "No doubt" },
       ],
+      correctAnswerId: "1",
     },
   ]
 
   const [currentIndex, setCurrentIndex] = useState(0)
-
+  const [correctAnswerScore, setCorrectAnswerScore] = useState(0)
+  
   const handleNext = () => {
     if (currentIndex < questions.length - 1) setCurrentIndex(currentIndex + 1)
   }
@@ -59,32 +64,79 @@ export default function Quiz() {
     if (currentIndex > 0) setCurrentIndex(currentIndex - 1)
   }
 
-  const handleSkip = () => {
-    setCurrentIndex((currentIndex + 1) % questions.length)
-  }
-  async function createCompletedSurvey() { }
-  const onSubmit = async (formData) => {
-    setState({})
-    try {
-      const response = await createCompletedSurvey({}, formData)
-      setState(response)
-    } catch (error) {
-      setState({ error: error.message })
+  // const handleSkip = () => {
+  //   setCurrentIndex((currentIndex + 1) % questions.length)
+  // }
+
+  function evaluateQuiz(formData) {
+    let correctAnswers = 0
+
+    const k = Object.keys(formData)
+    console.log("chikna", k)
+
+    for (const key in k) {
+      // console.log("key of formdata:", key)
+      // console.log("formData[k[key]]:", formData[k[key]])
+      // console.log(
+      //   "questions[key].correctAnswerId:",
+      //   questions[key].correctAnswerId
+      // )
+      if (formData[k[key]] === questions[key].correctAnswerId) {
+        console.log("harami", key)
+        correctAnswers = correctAnswers + 1
+      }
     }
+    return correctAnswers
+  }
+
+  
+
+  const onSubmit = async (formData) => {
+    const correctAnswerScore = evaluateQuiz(formData)
+
+    console.log("zalim", correctAnswerScore)
+    setCorrectAnswerScore(correctAnswerScore)
+    setState({})
+   
+      // const response = await createCompletedSurvey({}, formData)
+      setState({success:true})
+    
+    
+    
+    
+    
+    
+    
+
+
+
+    
   }
 
   const updateProgress = () => {
+    console.log("vilvvassi")
     const values = Object.values(getValues())
     const completedAnswers = values.filter((v) => v)
     setProgress((completedAnswers.length / questions.length) * 100)
   }
 
-  useEffect(() => {
-    updateProgress()
-  }, [currentIndex])
+
+
+
+
 
   useEffect(() => {
+    updateProgress()
+    console.log("maria", currentIndex)
+   
+  }, [currentIndex])
+
+
+
+  useEffect(() => {
+    console.log("churail",state)
     if (state.success) {
+      console.log("mulali", state)
       setCompleted(true)
     }
   }, [state])
@@ -105,31 +157,50 @@ export default function Quiz() {
             </div>
             <div className={styles.content}>
               <div className={`${styles.contentInner}`}>
-                <div
-                  className={`${styles.question} border border-black p-4`}
-                  key={questions[currentIndex]._id}
-                >
-                  <Controller
-                    control={control}
-                    name={`answer${questions[currentIndex]._id}`}
-                    render={({ field: { onChange } }) => (
-                      <RadioGroup
-                        label={
-                          <p>
-                            Q{currentIndex + 1}: {questions[currentIndex].name}
-                          </p>
-                        }
-                        onChange={onChange}
+                <div className="slider-container">
+                  <TransitionGroup component={null}>
+                    <CSSTransition
+                      key={currentIndex}
+                      timeout={900}
+                      classNames="slide-next"
+                    >
+                      <div
+                        className={`${styles.question} border border-black p-4`}
+                        key={questions[currentIndex]._id}
                       >
-                        {questions[currentIndex].answers.map((answer) => (
-                          <Radio key={answer._id} value={answer._id}>
-                            {answer.name}
-                          </Radio>
-                        ))}
-                      </RadioGroup>
-                    )}
-                    rules={{ required: true }}
-                  />
+                        <Controller
+                          control={control}
+                          name={`answer${questions[currentIndex]._id}`}
+                          render={({ field: { onChange } }) => (
+                            <RadioGroup
+                              label={
+                                <p>
+                                  Q{currentIndex + 1}:{" "}
+                                  {questions[currentIndex].name}
+                                </p>
+                              }
+                              onChange={onChange}
+                            >
+                              {questions[currentIndex].answers.map((answer) => (
+                                <Radio
+                                  onClick={() => {
+                                    currentIndex + 1 === questions.length
+                                      ? setProgress(100)
+                                      : null
+                                  }}
+                                  key={answer._id}
+                                  value={answer._id}
+                                >
+                                  {answer.name}
+                                </Radio>
+                              ))}
+                            </RadioGroup>
+                          )}
+                          rules={{ required: true }}
+                        />
+                      </div>
+                    </CSSTransition>
+                  </TransitionGroup>
                 </div>
 
                 <div className={`${styles.buttons} `}>
@@ -137,7 +208,7 @@ export default function Quiz() {
                     color="primary"
                     className="w-full"
                     isLoading={isSubmitting}
-                    type="submit"
+                    // type="submit"
                     onClick={() => {
                       handlePrevious()
                     }}
@@ -148,7 +219,8 @@ export default function Quiz() {
                     color="primary"
                     className="w-full"
                     isLoading={isSubmitting}
-                    type="submit"
+                    isDisabled={currentIndex === questions.length - 1}
+                    // type="submit"
                     onClick={() => {
                       handleNext()
                     }}
@@ -158,7 +230,7 @@ export default function Quiz() {
                 </div>
               </div>
             </div>
-            {/* <div className={styles.footer}>
+            <div className={styles.footer}>
               {state.error && <div className="error">{state.error}</div>}
               <div className={styles.footerInner}>
                 <Button color="primary" isLoading={isSubmitting} type="submit">
@@ -172,20 +244,23 @@ export default function Quiz() {
                   />
                 </div>
               </div>
-            </div> */}
+            </div>
           </div>
         </form>
       )}
 
       {completed && (
         <div className={styles.completed}>
-          <h1>Audit Completed!</h1>
-          <p className={styles.completedText}>
-            Thank you for your reflection. Your results are on the way to your
-            inbox. Feel free to contact your INSPIRE® expert for further
-            discussion.
+          <div className={""} style={{ marginBottom: "6px" }}>
+            <img src={"/public/res.svg"} width={100}></img>
+          </div>
+
+          <h1>Quiz Completed!</h1>
+          <p>Thank you for your reflection.</p>
+          <p>
+            Your quiz score is around {correctAnswerScore}/{questions.length}
           </p>
-          <h1 className={styles.completedFooter}>Your INSPIRE® Team</h1>
+          {/* <h1 className={styles.completedFooter}>Your INSPIRE® Team</h1> */}
         </div>
       )}
     </>
