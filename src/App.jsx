0d8b1224dbc4e1924fc7d6454bc567f3d9c1137f
Sadeem <sadeemasim12345@outlook.com>
2024-11-15 18:33:43 +0500

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-
-import "./App.css"
-import Main from "./pages/main/main"
-import Video from "./pages/video/video"
+import "./App.css";
+import Main from "./pages/main/main";
+import Video from "./pages/video/video";
 // import { useLocation } from "react-router-dom"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-import Quiz from "./pages/quiz/quiz"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Quiz from "./pages/quiz/quiz";
 function App() {
   return (
     <>
       {/* {!isAuthPage && <Navbar />}
       {!isAuthPage && <Sidebar />} */}
-      <AnimatePresence mode="wait">
-        <BrowserRouter>
+      <BrowserRouter>
+        <AnimatePresence mode="wait">
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/video" element={<Video />} />
             <Route path="/quiz" element={<Quiz />} />
           </Routes>
-        </BrowserRouter>
-      </AnimatePresence>
+        </AnimatePresence>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
