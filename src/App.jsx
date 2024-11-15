b81551887d2088c215b7264ee63b0a5f83e2ab68
Sadeem <sadeemasim12345@outlook.com>
2024-11-15 18:40:45 +0500

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Main from "./pages/main/main";
 import Video from "./pages/video/video";
 // import { useLocation } from "react-router-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import Quiz from "./pages/quiz/quiz";
 function App() {
   return (
